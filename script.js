ebca2ad07fcce4b44ab9b7b2d9a4b6e23f8205dd
Yogeshbairwa <ybairwa786@gmail.com>
2024-01-29 11:48:12 +0530

    Day3/section1/scrolling done

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //selections
 const body = document.querySelector(".body");
+const main = document.querySelector(".main");
 const nav = document.querySelector(".nav");
 const openAccount = document.querySelector(".btn-show--modal");
 const hiddenForm = document.querySelector(".hidden");
 const hiddenClose = document.querySelector(".hidden__close");
 const headertitle = document.querySelector(".header__title");
-const main = document.querySelector(".main");
-
+const hidden = document.querySelector(".hidden");
+const section1 = document.querySelector(".section--1");
+const learnMore = document.querySelector(".learnMore");
 console.log(hiddenClose);
 //functions
 const openForm = function () {
@@ -14,10 +16,27 @@ const openForm = function () {
   hiddenForm.style.opacity = 100;
 };
 const closeForm = function () {
-  hidden.style.opacity = 0;
-  main.style.opacity = 100;
+  // hidden.style.opacity = 0;
+  // main.style.opacity = 100;
+  console.log("hi");
 };
 
 //Events
 openAccount.addEventListener("click", openForm);
-hiddenClose.addEventListener("click", closeForm);
+hiddenClose.addEventListener("mousehover", closeForm);
+learnMore.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
